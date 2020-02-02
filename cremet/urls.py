a6fa1e3e6d4a496dc17ca03e377f200035817ed3
Pierre-Yves Gillier <pierre-yves.gillier@fr.clara.net>
2020-02-02 20:13:58 +0100

    [Back] Prepare Graphql GUI

--- cremet/urls.py
+++ cremet/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 from .settings import DEBUG
 
 from cremet.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=DEBUG, schema=schema)),
+    path('graphql/', csrf_exempt(
+        GraphQLView.as_view(graphiql=DEBUG, schema=schema))),
 ]
